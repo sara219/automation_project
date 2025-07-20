0c0c884ec63cd00f0d0df600c13acb14a9119a28
sara219 <sara97abuawwad@gmail.com>
2025-07-21 01:16:35 +0300

    tc

--- automation_script.py
+++ automation_script.py
@@ -1,27 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
 
 # => Setup The browser
 driver = webdriver.Chrome()
+baseUrl = "https://practicetestautomation.com/practice-test-login/"
+
+
+# Get element function/ function helper:
+def login_element(username, password):
+    driver.find_element(By.ID, "username").send_keys(username)
+    driver.find_element(By.ID, "password").send_keys(password)
+    driver.find_element(By.ID, "submit").click()
+
 
 # ! Test Case 001: Valid Login:
 try:
     print(" ➡ TC001: Testing Valid Login ...")
     # navigate to the login page
-    driver.get("https://practicetestautomation.com/practice-test-login/")
-    sleep(1)
+    driver.get(baseUrl)
 
     # Enter VALID credentials
-    username_input = driver.find_element(By.ID, "username")
-    password_input = driver.find_element(By.ID, "password")
-    login_btn = driver.find_element(By.ID, "submit")
-
-    username_input.send_keys("student")
-    password_input.send_keys("Password123")
-    login_btn.click()
+    login_element("student", "Password123")
 
     # success message:
     success_message = driver.find_element(By.CLASS_NAME, "post-title").text
@@ -33,28 +36,22 @@ try:
     assert logout_btn.is_displayed()
     print("✅ LOGOUT BTN FOUND!")
     # TC001: Pass
-    print("✅✅ Login Test Case 001: Passes")
+    print("✅✅ TC001: Passes")
 
 except Exception as error:
     # TC001: Fail
-    print("❌ Login Test Case 001: Failed", error)
+    print("❌ TC001: Failed", error)
 
 # -------------------------------------------------
 # ! Test Case 002: Login with invalid Username:
 try:
     print(" ➡ TC002: Testing Login with Invalid Username...")
     # navigate to the login page
-    driver.get("https://practicetestautomation.com/practice-test-login/")
+    driver.get(baseUrl)
     sleep(5)
 
     # Enter INVALID credentials: wrong username
-    username_input = driver.find_element(By.ID, "username")
-    password_input = driver.find_element(By.ID, "password")
-    login_btn = driver.find_element(By.ID, "submit")
-
-    username_input.send_keys("studt")
-    password_input.send_keys("Password123")
-    login_btn.click()
+    login_element("studt", "Password123")
 
     # capture the error message
     error_msg = WebDriverWait(driver, 5).until(
@@ -64,12 +61,12 @@ try:
     print("⛔️ Error Message:", error_msg.text)
 
     assert "your username is invalid" in error_msg.text.lower()
-    print("✅✅ Login Test Case 002: Passes")
+    print("✅✅ TC002: Passes")
 
 
 except Exception as error:
     # TC002: Fail
-    print("❌ Login Test Case 002: Failed", error)
+    print("❌ TC002: Failed", error)
 
 # -------------------------------------------------
 # ! Test Case 003: Login with invalid Password:
@@ -77,20 +74,14 @@ try:
     print("➡ TC003: Testing Login with Invalid Password...")
 
     # navigate to login page
-    driver.get("https://practicetestautomation.com/practice-test-login/")
+    driver.get(baseUrl)
     sleep(1)
 
     # Enter VALID username, INVALID password
-    username_input = driver.find_element(By.ID, "username")
-    password_input = driver.find_element(By.ID, "password")
-    login_btn = driver.find_element(By.ID, "submit")
-
-    username_input.send_keys("student")
-    password_input.send_keys("password1245")
-    login_btn.click()
+    login_element("student", "password124")
 
     # Wait for error message
-    err_msg = WebDriverWait(driver, 5).until(
+    err_msg = WebDriverWait(driver, 6).until(
         EC.visibility_of_element_located((By.ID, "error"))
     )
 
@@ -99,7 +90,52 @@ try:
 
     # Updated assertion
     assert "your password is invalid" in err_msg.text.lower()
-    print("✅✅ Login Test Case 003: Passes")
+    print("✅✅ TC003: Passes")
 
 except Exception as error:
-    print("❌ Login Test Case 003: Failed", error)
+    print("❌ TC003: Failed", error)
+
+# -------------------------------------------------
+# ! Test Case 004: Access Protected Page:
+try:
+    print("➡ TC004: Access Protected Page ...")
+
+    driver.get("https://practicetestautomation.com/logged-in-successfully/")
+    success_msg = WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "post-title"))
+    )
+    assert "logged in successfully" in success_msg.text.lower()
+    print("✅✅ TC004: Passed")
+
+except Exception as error:
+    print("❌ TC004: Failed", error)
+
+# -------------------------------------------------
+# ! Test Case 005: Logout:
+
+try:
+    print("➡ TC005: Logout and Session End ...")
+    driver.get(baseUrl)
+
+    # Enter VALID credentials
+    login_element("student", "Password123")
+    logout_btn = driver.find_element(By.LINK_TEXT, "Log out")
+    assert logout_btn.is_displayed()
+    print("✅ logout btn found!")
+
+    # logout
+    logout_btn.click()
+    # Check redirection to login page
+    current_url = driver.current_url
+    assert "practice-test-login" in current_url.lower()
+
+    # title = driver.find_element(By.TAG_NAME, "h1").text
+    # assert "test login" in title.lower()
+    print("✅✅ TC005: Passed")
+
+
+except Exception as error:
+    print("❌ TC005: Failed", error)
+
+driver.quit()
+print("Browser closed. Test completed.")
